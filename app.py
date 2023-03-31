03ac1205e7932600cbd43a309109e1f5ebd9d7e6
midhunsankar23 <midhunsankarprotonmail@gmail.com>
2023-04-01 04:37:45 +0530

    next update, not working thoigh

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import subprocess
+import time
 from flask import Flask, jsonify, request, send_from_directory, redirect, url_for, flash, render_template
+from threading import Thread
 import os
 from werkzeug.utils import secure_filename
+import pika
 
 
 app = Flask(__name__)
@@ -9,6 +13,11 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='image_queue')
 
 
 def allowed_file(filename):
@@ -39,7 +48,16 @@ def insert_image():
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        #print('upload_image filename: ' + filename)
+        # Open a connection to RabbitMQ server
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters('localhost'))
+        channel = connection.channel()
+        # Declare a queue named 'image_queue'
+        channel.queue_declare(queue='image_queue')
+        # Publish the file to the 'image_queue' queue
+        channel.basic_publish(
+            exchange='', routing_key='image_queue', body=file.read())
+        connection.close()
         flash('Image successfully uploaded and displayed below')
         return send_from_directory('ui', 'index.html')
     else:
@@ -51,6 +69,33 @@ def insert_image():
 def get_favicon():
     return send_from_directory('favicon', 'favicon.ico')
 
+# Define a callback function to handle incoming messages from the 'image_queue'
+def process_image(ch, method, properties, body):
+    # Create a unique filename for the image
+    filename = secure_filename('image_' + str(time.time()) + '.jpg')
+    # Save the image to disk
+    with open(os.path.join(app.config['UPLOAD_FOLDER'], filename), 'wb') as f:
+        f.write(body)
+    # Run the image through the YOLO model using Darknet
+    darknet_path = 'darknet'
+    cfg_path = '/darknet/cfg/yolov3.cfg'
+    weights_path = '/darknet/yolov3.weights'
+    cmd = [os.path.join(darknet_path, 'darknet'), 'detect', cfg_path, weights_path, os.path.join(app.config['UPLOAD_FOLDER'], filename)]
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # Extract the labels from the output and write them to a text file
+    labels = [line.decode('utf-8').strip() for line in result.stdout.splitlines()]
+    with open(os.path.join(app.config['UPLOAD_FOLDER'], filename + '.txt'), 'w') as f:
+        f.write('\n'.join(labels))
+    # Acknowledge the message
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+# Set up the consumer to consume messages from the 'image_queue'
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='image_queue', on_message_callback=process_image)
+
+# Start consuming messages from the 'image_queue'
+channel.start_consuming()
+
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
@@ -58,4 +103,7 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--port', type=int, default=5000)
     args = parser.parse_args()
     port = args.port
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
+    
+
+# TODO - fix same name for image file
